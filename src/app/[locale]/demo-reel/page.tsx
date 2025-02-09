0b9138f0a6c44af88b124702e73a1ec8efa43deb
Gregory Albert <gregoryalbert1209@gmail.com>
2025-02-09 15:39:51 +0400

    Add demo reel

--- src/app/[locale]/demo-reel/page.tsx
+++ src/app/[locale]/demo-reel/page.tsx
@@ -1,23 +1,13 @@
-import Link from "next/link";
-
 export default function ProjectPage() {
   return (
     <div className="flex flex-col gap-4 items-center w-full">
-      <p className="font-bold text-5xl">Demo Reel</p>
-      <p className="text-gray-400 sm:text-xl text-base text-center">
-        Overview of my past work{" "}
-      </p>
       <div className="flex items-center justify-center w-full">
-        <div className="bg-gray-300 rounded-sm p-8 sm:p-36 my-6">
-        <Link
-          href="https://www.youtube.com/channel/UC3MvvojiP4AW5yigMiLQQig"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="text-black text-xl hover:text-red-900"
-        >
-          Visit My YouTube Channel
-        </Link>
-        </div>
+        <iframe
+        className="w-full min-h-[500px]"
+          src="https://www.youtube.com/embed/iYjQkFmQ1iU?autoplay=1&loop=1&playlist=iYjQkFmQ1iU&rel=0&showinfo=0&modestbranding=1"
+          allow="autoplay; encrypted-media"
+          allowFullScreen
+        />
       </div>
     </div>
   );
